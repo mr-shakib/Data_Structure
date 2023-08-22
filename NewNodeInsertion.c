6664a01cc96863b1f7afc074d2dc08264ea6ad6b
mr-shakib <132547096+mr-shakib@users.noreply.github.com>
2023-08-22 23:26:26 +0600

    Insert new node in linked list
    Create a linked list and insert new node at beginning, end or Nth position in C.

---
+++ NewNodeInsertion.c
@@ -0,0 +1,104 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+} *head, *temp;
+
+void createList(int n)
+{
+    struct node *newnode;
+
+    head = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the value of node-1 : ");
+    scanf("%d", &head->data);
+    head->next = NULL;
+    temp = head;
+    for (int i = 1; i < n; i++)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter the value of node-%d : ", i + 1);
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+        temp->next = newnode;
+        temp = newnode;
+    }
+}
+
+void InsertAtBeg()
+{
+
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the value of new(first) node : ");
+    scanf("%d", &newnode->data);
+    newnode->next = head;
+    head = newnode;
+}
+
+void InsertAtEnd()
+{
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    printf("Enter the data of new(last) node : ");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
+    temp->next = newnode;
+}
+
+void InsertByPosition(int pos)
+{
+    struct node *newnode;
+
+    temp = head;
+
+    for (int i = 1; i < pos - 1; i++)
+    {
+        temp = temp->next;
+    }
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the value of new node at position-%d : ",pos);
+    scanf("%d",&newnode->data);
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
+
+void displayList()
+{
+    int count = 0;
+    struct node *temp;
+    temp = head;
+    while (temp != NULL)
+    {
+        count++;
+        printf("Value of node - %d  = %d\n", count, temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int n, pos;
+    printf("Enter the numebr of elements of the list : ");
+    scanf("%d", &n);
+    createList(n);
+    displayList();
+    InsertAtBeg();
+    displayList();
+    InsertAtEnd();
+    displayList();
+    printf("Enter the position you want to add data : ");
+    scanf("%d", &pos);
+    InsertByPosition(pos);
+    displayList();
+
+    return 0;
+}
